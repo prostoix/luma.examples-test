ef8d833d7935d03023cc5d87426a664af79b15f8
prostoix <123619481+prostoix@users.noreply.github.com>
2024-03-06 12:06:42 +0000

    465

--- examples/loby_text.py
+++ examples/loby_text.py
@@ -1,27 +1,15 @@
-import time
-from pathlib import Path
 from luma.core.interface.serial import i2c
-from luma.core.render import canvas
 from luma.oled.device import ssd1306
+from luma.core.render import canvas
 from PIL import ImageFont
 
-#Define the I2C serial interface and the SSD1306 device
-serial = i2c(port=0, address=0x3C)
+serial = i2c(device=0, address=0x3C)
 device = ssd1306(serial)
 
-#Define a custom font
-font_path = "SAC.ttf" 
-custom_font = ImageFont.truetype(font_path, 12)
+font_path = "SAC.ttf"
+font = ImageFont.truetype(font_path, 12)
 
-#Display text using the custom font
-device.clear() 
-device.text((0, 0), "Hello, World!", fill="white", font=custom_font) 
-device.show()
+text = "Hello, World!"
 
-time.sleep(5) # Display text for 5 seconds
-
-#Clear the display
-device.clear() 
-device.show()
-
-#cp tmp/luma.examples-test/examples/fonts/SAC.ttf /usr/share/fonts/SAC.ttf
+with canvas(device) as draw:
+    draw.text((0, 0), text, font=font, fill="white")
