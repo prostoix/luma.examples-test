a4cac695b955249afdd90c6365984aa538c8db81
prostoix <123619481+prostoix@users.noreply.github.com>
2024-03-06 11:29:59 +0000

    fdd

--- examples/loby_text.py
+++ examples/loby_text.py
@@ -1,13 +1,19 @@
 import time
+from pathlib import Path
 from luma.core.interface.serial import i2c
 from luma.core.render import canvas
 from luma.oled.device import ssd1306
+from PIL import ImageFont
 
 def do_nothing(obj):pass
 
 serial = i2c(port=0, address=0x3C)
 device = ssd1306(serial)
 device.cleanup = do_nothing
+
+def make_font(name, size):
+    font_path = str(Path(__file__).resolve().parent.joinpath('fonts', name))
+    return ImageFont.truetype(font_path, size)
 
 def primitives(device, draw):
     # Draw some shapes
@@ -16,17 +22,17 @@ def primitives(device, draw):
     shape_width = 20
     top = padding
     bottom = device.height - padding - 1
-
+    fonts = [make_font("SAC.ttf", sz) for sz in range(24, 8, -2)]
     # Move left to right keeping track of the current x position for drawing shapes
-    x = padding
+    #x = padding
 
     # Write two lines of text
-    left, t, right, bottom = draw.textbbox((0, 0), 'World!!!')
-    w, h = right - left, bottom - t
-    x = device.width - padding - w
+    #left, t, right, bottom = draw.textbbox((0, 0), 'World!!!')
+    #w, h = right - left, bottom - t
+    #x = device.width - padding - w
     #draw.rectangle((x, top + 4, x + w, top + h), fill="black")
     #draw.rectangle((x, top + 16, x + w, top + 16 + h), fill="black")
-    draw.text((30, 40), 'Hello', fill="cyan")
+    draw.text((30, 40), 'Hello', font=fonts, fill="cyan")
     #draw.text((device.width - padding - w, top + 4), 'Hello', fill="cyan")
     #draw.text((device.width - padding - w, top + 16), 'World!', fill="purple")
 
