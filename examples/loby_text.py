8d7aec55b0fba881bd628a6a5ddef2b521a1e8e0
prostoix <123619481+prostoix@users.noreply.github.com>
2024-03-06 12:26:54 +0000

    fd

--- examples/loby_text.py
+++ examples/loby_text.py
@@ -10,14 +10,16 @@ serial = i2c(port=0, address=0x3C)
 device = ssd1306(serial)
 
 font_path = "/usr/share/fonts/SAC.ttf"
-font = ImageFont.truetype(font_path, 20)
+font_h1 = ImageFont.truetype(font_path, 40)
+font_h2 = ImageFont.truetype(font_path, 20)
+font_h3 = ImageFont.truetype(font_path, 12)
 
 name = "Piano"
 number = "001"
 collection = "1"
 
 with canvas(device) as draw:
-    draw.text((10, 0), name, font=font, fill="white")
-    draw.text((10, 5), number, font=font, fill="white")
-    draw.text((60, 10), collection, font=font, fill="white")
-time.sleep(5) 
+    draw.text((10, 0), name, font=font_h3, fill="white")
+    draw.text((10, 5), number, font=font_h1, fill="white")
+    draw.text((60, 10), collection, font=font_h3, fill="white")
+time.sleep(15) 
