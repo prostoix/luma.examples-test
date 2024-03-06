4adf76a4db53913d7284eec288e7d577cd29a6ef
prostoix <123619481+prostoix@users.noreply.github.com>
2024-03-06 11:45:17 +0000

    sd

--- examples/loby_text.py
+++ examples/loby_text.py
@@ -6,7 +6,7 @@ from luma.oled.device import ssd1306
 from PIL import ImageFont
 
 #Define the I2C serial interface and the SSD1306 device
-serial = i2c(port=1, address=0x3C) 
+serial = i2c(port=0, address=0x3C)
 device = ssd1306(serial)
 
 #Define a custom font
